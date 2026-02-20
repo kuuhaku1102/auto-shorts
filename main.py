8478dfc0ee196095ded94427a2f0527c6cf23d37
kuuhaku1102 <j-@infintydesign.online>
2026-02-21 00:07:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from ranking_scraper import get_top5
 from detail_scraper import get_card_name
 
+from db.database import init_db, insert_record
+from utils import clean_price, clean_rate
+
+
 def run(mode, label):
     print(f"\n=== {label} ===")
 
@@ -9,13 +13,23 @@ def run(mode, label):
     for card in cards:
         name = get_card_name(card["detail_url"])
 
-        print({
+        price = clean_price(card["price"])
+        rate = clean_rate(card["change_rate"])
+
+        data = {
             "rank": card["rank"],
             "name": name,
-            "price": card["price"],
-            "change_rate_7d": card["change_rate"]
-        })
+            "price": price,
+            "change_rate": rate,
+            "detail_url": card["detail_url"]
+        }
+
+        print(data)
+
+        insert_record(mode, data)
+
 
 if __name__ == "__main__":
+    init_db()
     run(5, "7日高騰TOP5")
     run(6, "7日下落TOP5")
